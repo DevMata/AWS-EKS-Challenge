6c7eabf69b67a150968c4f28e50ebb1ff76fe651
Antonio Mata <antonio.mata.sv@gmail.com>
2021-05-10 19:00:46 -0600

    chore(KRKY-1): add helmet and connect-timeout

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core'
+import * as helmet from 'helmet'
+import * as timeout from 'connect-timeout'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(timeout('30s'))
+  app.use(helmet())
+
   await app.listen(3000)
 }
 bootstrap()
