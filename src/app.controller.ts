1322765faf5c00360f8eda2e67a89430be21239e
Antonio Mata <antonio.mata.sv@gmail.com>
2021-05-11 23:37:14 -0600

    feat(KRKY-4): implement health checking

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
+import {
+  HealthCheck,
+  HealthCheckResult,
+  HealthCheckService,
+  HttpHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private readonly httpHealthIndicator: HttpHealthIndicator,
+    private readonly typeOrmHealthIndicator: TypeOrmHealthIndicator,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @HealthCheck()
+  @Get('health')
+  check(): Promise<HealthCheckResult> {
+    return this.healthCheckService.check([
+      () => this.httpHealthIndicator.pingCheck('github', 'https://github.com'),
+      () => this.typeOrmHealthIndicator.pingCheck('database'),
+    ])
   }
 }
