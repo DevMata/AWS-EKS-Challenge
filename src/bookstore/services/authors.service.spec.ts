a222ab2cfa31a04a0de4f2cabeba5a69a94d2f8b
Antonio Mata <antonio.mata.sv@gmail.com>
2021-05-11 19:01:29 -0600

    test(KRKY-3): ensure findTopAuthors works properly

--- src/bookstore/services/authors.service.spec.ts
+++ src/bookstore/services/authors.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { NotFoundException } from '@nestjs/common'
+import { name, random, finance } from 'faker'
 import { AuthorsRepository } from '../repositories/authors.repository'
 import { SalesItemsRepository } from '../repositories/sales-items.repository'
 import { AuthorsService } from './authors.service'
@@ -7,6 +8,7 @@ import { AuthorsService } from './authors.service'
 describe('AuthorsService', () => {
   let authorsService: AuthorsService
   let authorsRepository: AuthorsRepository
+  let salesItemsRepository: SalesItemsRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,6 +27,7 @@ describe('AuthorsService', () => {
 
     authorsService = module.get<AuthorsService>(AuthorsService)
     authorsRepository = module.get(AuthorsRepository)
+    salesItemsRepository = module.get(SalesItemsRepository)
   })
 
   it('should be defined', () => {
@@ -38,6 +41,21 @@ describe('AuthorsService', () => {
       await expect(
         authorsService.findTopAuthors({ author_name: 'any_name' }),
       ).rejects.toThrow(NotFoundException)
+    })
+
+    it('should return an array of authors', async () => {
+      const authors = [...Array(5)].map(() => ({
+        id: random.number(),
+        name: `${name.firstName()} ${name.lastName()}`,
+        revenue: finance.amount(),
+      }))
+
+      jest
+        .spyOn(salesItemsRepository, 'findTopAuthors')
+        .mockResolvedValue(authors)
+
+      const topAuthors = await authorsService.findTopAuthors({})
+      expect(topAuthors).toHaveLength(5)
     })
   })
 })
